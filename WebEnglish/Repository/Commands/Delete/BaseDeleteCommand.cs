01375f7c4d8ddb28a631281e8a7935fd08736350
Georgy <mr.smirnovgd@yandex.ru>
2025-06-19 13:36:22 +0500

    RemoveCommand is not Tested

---
+++ WebEnglish/Repository/Commands/Delete/BaseDeleteCommand.cs
@@ -0,0 +1,59 @@
+﻿using ContextDataBase;
+using Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Repository.DTO;
+using StatusGeneric;
+
+namespace Repository.Commands.Delete;
+
+public record BaseDeleteCommand(BaseDto EntityToRemove) : BaseCommandDataBase();
+
+public sealed record DeleteWordRequest(WordDto WordToRemove) : BaseDeleteCommand(WordToRemove)
+{
+    public override string ToString()
+    {
+        string name = $"{nameof(DeleteWordRequest)} {WordToRemove.Id}";
+        return base.ToString();
+    }
+}
+
+public record DeleteWordRequestHandler : IRequestHandler<DeleteWordRequest, IStatusGeneric>
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<DeleteWordRequestHandler> _logger;
+
+    public DeleteWordRequestHandler(IServiceProvider provider, ILogger<DeleteWordRequestHandler> logger)
+    {
+        _serviceProvider = provider;
+        _logger = logger;
+    }
+
+    public async Task<IStatusGeneric> Handle(DeleteWordRequest request, CancellationToken cancellationToken)
+    {
+        string msg = $"{nameof(DeleteWordRequestHandler)} {request}";
+        _logger.LogInformation($"{msg}");
+        StatusGenericHandler status = new StatusGenericHandler()
+        {
+            Header = msg
+        };
+        await using var scope = _serviceProvider.CreateAsyncScope();
+        var provider = scope.ServiceProvider;
+        try
+        {
+            var context = provider.GetRequiredService<AppDbContext>();
+            var toRemove = await context.Words.SingleAsync(x => x.Id == request.WordToRemove.Id, cancellationToken: cancellationToken);
+            toRemove.SoftDeleted = true;
+            await context.SaveChangesAsync(cancellationToken);
+            _logger.LogInformation($"{request} is success");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.ToString());
+            status.AddError(e, e.Message);
+        }
+        return status;
+    }
+}
---
+++ WebEnglish/Repository/Commands/Update/UpdateWordRequest.cs
@@ -0,0 +1,11 @@
+﻿using Repository.DTO;
+
+namespace Repository.Commands.Update;
+
+public sealed record UpdateWordRequest(WordDto Modified) : BaseUpdateCommand<WordDto>(Modified)
+{
+    public override string ToString()
+    {
+        return $"{nameof(UpdateWordRequest)} Id: {Modified.Id} Eng:{Modified.Eng} Rus:{Modified.Rus}";
+    }
+}
